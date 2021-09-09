092dc4c4ffcb5ace3f42b28f44ff4511edd66a95
wlsgh7608 <wlsgh7608@naver.com>
2021-09-09 18:24:59 +0900

    Add liking field

--- tweets/models.py
+++ tweets/models.py
@@ -5,14 +5,22 @@ import random
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User,  on_delete = models.CASCADE) # many users can many tweets
+    tweet = models.ForeignKey("Tweet",  on_delete = models.CASCADE) # many users can many tweets
+    timestamp = models.DateTimeField(auto_now_add = True)
+ 
 class Tweet(models.Model):
     # SQL
     # id = models.AutoField(pk = True)
     # user가 삭제될 때 user가 쓴 tweet 모두 삭제(cascade)
     user = models.ForeignKey(User,  on_delete = models.CASCADE) # many users can many tweets
+    likes = models.ManyToManyField(User,related_name='tweet_user',blank = True ,through = TweetLike)
+
     content  = models.TextField(blank = True, null = True)
     image = models. FileField(upload_to = 'images/', blank = True , null = True)
-    
+    timestamp = models.DateTimeField(auto_now_add = True)
+ 
 
     # def __str__(self):
     #     return self.content
