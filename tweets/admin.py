8b27521993c5e767936d73f5512d689f324959b8
wlsgh7608 <wlsgh7608@naver.com>
2021-09-11 17:20:56 +0900

    Like & Unlike  & Retweet Btn

--- tweets/admin.py
+++ tweets/admin.py
@@ -11,6 +11,7 @@ class TweetLikeAdmin(admin.TabularInline):
 class TweetAdmin(admin.ModelAdmin):
     inlines = [TweetLikeAdmin]
     list_display =['__str__','user']
+
     search_fields = ['content','user__username','user__email']
     class Meta:
         model = Tweet
